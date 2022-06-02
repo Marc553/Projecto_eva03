f7ce140fc7e77fac0451b06d868c80a886dfd209
Marc553 <91455235+Marc553@users.noreply.github.com>
2022-06-02 07:53:15 +0200

    31/05/2022

--- Assets/Scripts/Enemy_cabolo.cs
+++ Assets/Scripts/Enemy_cabolo.cs
@@ -4,32 +4,43 @@ using UnityEngine;
 using UnityEngine.AI;
 
 public class Enemy_cabolo : MonoBehaviour
-{
+{/*
+    #region VARIALBES
+    // Variables cogen datos del propio enemigo
     private NavMeshAgent agent;
     private Rigidbody enemyRigidbody;
     private Transform player;
     [SerializeField] private float speed = 5f;
+ 
+    // Variables del limite de sala 
+    private float deteccionLimSala = 40f;
+    private bool playerDentroSala;
+    [SerializeField] private LayerMask capasLimites;
 
     #region CODIGO AGENTE
 
-    //CODIGO DEL MODO AGENTE
-    private float visionRange = 20;
-    private float attackRange = 10;
+    // Distancias de las areas
+    private float areaVisionAg = 20;
+    private float areaAtaqueAg = 10;
 
-    private bool playerInVisionRange;
-    private bool playerInAttackRange;
+    // Bool de si el player esta dentro
+    private bool pruevaVisionAg;
+    private bool pruevaAtaqueAg;
 
     // Capa que debe tenerse en cuenta para campos de visión y ataque
     [SerializeField] private LayerMask playerLayer;
 
     // Ataque
-    [SerializeField] private GameObject bullet;
-    private float timeBetweenAttacks = 2f;
-    private bool canAttack = true;
-    private float upAttackForce = 5f;
-    private float forwardAttackForce = 8f;
+    [SerializeField] private GameObject projectil;
+    private float tiempoAtaque = 2f;
+    private bool puedePegar = true;
+    private float fuerzaAtaqueArriba = 5f;
+    private float fuerzaAtaque = 8f;
     #endregion
 
+    #endregion
+
+    #region METODOS
     private void Awake()
     {
         // Guardamos la referencia a la componente NavMeshAgent del Game Object
@@ -38,6 +49,7 @@ public class Enemy_cabolo : MonoBehaviour
 
     void Start()
     {
+        // Enlazo las variables con los componentes del enemy
         enemyRigidbody = GetComponent<Rigidbody>();
         player = GameObject.Find("Player").transform;
     }
@@ -45,96 +57,70 @@ public class Enemy_cabolo : MonoBehaviour
 
     void Update()
     {
-        // Campos de visión y ataque del agente 
+        // Areas de visión/ataque del agente
+        
+        // Detecta si el player esta dentro de la sala para empezar persecucion/ataque
         Vector3 pos = transform.position;
-        playerInVisionRange = Physics.CheckSphere(pos, visionRange, playerLayer);//mirar de usar el Check cube con forma de ortoedro 
-        playerInAttackRange = Physics.CheckSphere(pos, attackRange, playerLayer);
+        playerDentroSala = Physics.SphereCast(pos, 0.5f, -transform.right, out RaycastHit hit, deteccionLimSala, capasLimites);
+        pruevaAtaqueAg = Physics.CheckSphere(pos, areaAtaqueAg, playerLayer);
 
         // Activamos la lógica de persecución 
-        if (playerInVisionRange && !playerInAttackRange) //ni en area de vision pero no ataque
+        if (playerDentroSala && !pruevaAtaqueAg) //ni en area de vision pero no ataque
         {
             Chase();//son paretes del component agent
         }
 
-        if (playerInVisionRange && playerInAttackRange) //en area de ataque ni de vision
+        if (playerDentroSala && pruevaAtaqueAg) //en area de ataque ni de vision
         {
             Attack();//son paretes del component agent
         }
     }
-    #region RAY CAST
-
-    private bool VisionPlayer()
-    {
-        Vector3 ray = transform.position;
-
-        float marcaRasho = 10000;
-
-        /*Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * marcaRasho, Color.yellow);
-
-        RaycastHit infoHit;
-
-        if (Physics.Raycast(ray, out infoHit, marcaRasho))
-        {
-            return 
-        }*/
-
-        Physics.Raycast(ray, transform.forward, out RaycastHit infoHit, marcaRasho);
-
-        Color raycastColor = infoHit.collider != null ? Color.green : Color.black;
-
-        Debug.DrawRay(ray, Vector3.forward * marcaRasho, raycastColor, 0, false);
-        return infoHit.collider != null; 
-    }
 
     #endregion
 
-    // Lógica de persecución del agente
+    #region FUNCIONES
+    // Lógica de persecución (Agente)
     private void Chase()
     {
         // Fijamos como objetivo al player
         transform.LookAt(player);
-        agent.SetDestination(player.position);//eta atado al if de antes
+        agent.SetDestination(player.position);
         
     }
 
-    // Lógica de ataque del agente
+    // Lógica de ataque (Agente)
     private void Attack()
     {
-        // Paramos al agente para cuando ataque 
-        agent.SetDestination(transform.position);
-        transform.LookAt(player.position);
-
         // Si hemos finalizado el Attack Cooldown
-        if (canAttack)
+        if (puedePegar)
         {
             // Disparamos bala con físicas
-            Rigidbody rb = Instantiate(bullet, transform.position, Quaternion.identity).GetComponent<Rigidbody>(); //instanciamiento de la bala para disaparar al player
-            rb.AddForce(transform.forward * forwardAttackForce, ForceMode.Impulse); //disparo de la bala, messirve que dispare la bala con fuerzsas, asi se caen 
-            rb.AddForce(transform.up * upAttackForce, ForceMode.Impulse);
+            Rigidbody rb = Instantiate(projectil, transform.position, Quaternion.identity).GetComponent<Rigidbody>(); //instancia la bala para disaparar al player
+            rb.AddForce(transform.forward * fuerzaAtaque, ForceMode.Impulse); //disparo de la bala
 
             // Activamos Attack Cooldown
-            canAttack = false;
+            puedePegar = false;
             StartCoroutine(AttackCooldown());
         }
     }
     private IEnumerator AttackCooldown()
     {
-        yield return new WaitForSeconds(timeBetweenAttacks);
-        canAttack = true;
+        yield return new WaitForSeconds(tiempoAtaque);
+        puedePegar = true;
     }
 
-    // Esta función nos permite dibujar Gizmos
+    // Gizmos
     private void OnDrawGizmos()
     {
-        // Rango de visión del agente
-        Gizmos.color = Color.green;
-        Gizmos.DrawWireSphere(transform.position, visionRange);//usar el DrawWireCube( deformar como ortoedro)
+        // Rango de ataque (Agente)
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, areaAtaqueAg);
 
-        // Rango de ataque del agente
-        Gizmos.color = Color.magenta;
-        Gizmos.DrawWireSphere(transform.position, attackRange);
+        // Rango de vision del  limite de la sala (Agente)
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, deteccionLimSala);
     }
-
-
+    
+    #endregion
+    */
 }
-
--- Assets/Scripts/Enemy_canon.cs
+++ Assets/Scripts/Enemy_canon.cs
@@ -1,41 +1,106 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy_canon : MonoBehaviour
 {
-    private GameObject player;
+    #region VARIALBES
+    // Variables cogen datos del propio enemigo
+    
+    private Rigidbody enemyRigidbody;
+    private Transform player;
+    [SerializeField] private float speed = 5f;
 
+    // Variables del limite de sala 
+    private float deteccionLimSala = 40f;
+    private bool playerDentroSala;
+    [SerializeField] private LayerMask capasLimites;
+
+    #region CODIGO AGENTE
+
+    // Distancias de las areas
+    private float areaVisionAg = 20;
+    private float areaAtaqueAg = 10;
+
+    // Bool de si el player esta dentro
+    private bool pruevaVisionAg;
+    private bool pruevaAtaqueAg;
+
+    // Capa que debe tenerse en cuenta para campos de visión y ataque
+    [SerializeField] private LayerMask playerLayer;
+
+    // Ataque
+    [SerializeField] private GameObject projectil;
+    private float tiempoAtaque = 2f;
+    private bool puedePegar = true;
+    private float fuerzaAtaqueArriba = 5f;
+    private float fuerzaAtaque = 20f;
+    #endregion
+
+    #endregion
+
+    #region METODOS
+   
     void Start()
     {
-        player = GameObject.Find("Player");
+        // Enlazo las variables con los componentes del enemy
+        enemyRigidbody = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player").transform;
     }
 
-    private void Update()
+
+    void Update()
+    {
+        // Areas de visión/ataque del agente
+
+        // Detecta si el player esta dentro de la sala para empezar persecucion/ataque
+        Vector3 pos = transform.position;
+        playerDentroSala = Physics.SphereCast(pos, 0.5f, transform.forward, out RaycastHit hit, deteccionLimSala, capasLimites);
+
+        //playerDentroSala = Physics.CheckSphere(pos, deteccionLimSala, capasLimites);
+
+        // Activamos la lógica de persecución 
+        
+        if (playerDentroSala) //en area de ataque ni de vision
+        {
+            Attack();//son paretes del component agent
+        }
+    }
+
+    #endregion
+
+    #region FUNCIONES
+
+    // Lógica de ataque (Agente)
+    private void Attack()
     {
         transform.LookAt(player.transform.position);
-        Debug.Log(VisionPlayer());
-    }
-
-
-        private bool VisionPlayer()
+        // Si hemos finalizado el Attack Cooldown
+        if (puedePegar)
         {
-        Ray ray = new Ray(transform.position, transform.forward);
+            // Disparamos bala con físicas
+            Rigidbody rb = Instantiate(projectil, transform.position, Quaternion.identity).GetComponent<Rigidbody>(); //instancia la bala para disaparar al player
+            rb.AddForce(transform.forward * fuerzaAtaque, ForceMode.Impulse); //disparo de la bala
 
-        float marcaRasho = 10000; 
-
-        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * marcaRasho, Color.yellow);
-
-        RaycastHit infoHit;
-
-        if (Physics.Raycast(ray, out infoHit, marcaRasho))
-        {  
-            
-            return infoHit.collider.CompareTag("Player");
-        }
-        else
-        {
-            return false;
-        }
+            // Activamos Attack Cooldown
+            puedePegar = false;
+            StartCoroutine(AttackCooldown());
         }
     }
+    private IEnumerator AttackCooldown()
+    {
+        yield return new WaitForSeconds(tiempoAtaque);
+        puedePegar = true;
+    }
+
+    // Gizmos
+    private void OnDrawGizmos()
+    {
+        // Rango de vision del  limite de la sala (Agente)
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, deteccionLimSala);
+    }
+
+    #endregion
+}
